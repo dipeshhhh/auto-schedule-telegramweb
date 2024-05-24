389c6f29b95e6923279a16d4c1c07f283f93bb45
Dipesh <kvdipesh@gmail.com>
2024-05-24 21:18:13 +0530

    add shortcut to stop execution and bugfixes
    Updates:
    - Added shortcut key ALT+X to stop execution at any given time.
    
    Bugfixes:
    - Fixed upperlimit for hours and minutes checks, now wrong hours that go above 23 and minutes that go above 59 are countered.
    - Added condition to prevent sending message with time before current time.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function main() {
+async function main() {
   // Utility function to create a delay
   function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -24,6 +24,10 @@ function main() {
     timeInputs[1].value = `${mm}`;
   }
 
+  // Function to check if the current message time is before the specified time
+  function isLateMsg(msgHr, msgMin, minHr, minMin) {
+    return ((msgHr < minHr) || (msgHr === minHr && msgMin < minMin));
+  }
   // Function to automate sending messages
   async function autoSend(messages, fromHr = 0, fromMin = 0) {
     // jsonInp must be a list of js objects
@@ -33,15 +37,22 @@ function main() {
     fromMin = typeof fromMin === 'number' ? fromMin : Number.parseInt(fromMin);
     const delay = 500;
 
-    if (!confirm(`Sending messages from: ${fromHr}:${fromMin}.\n Please confirm to continue.`)) return;
+    const confirmationMessage = (fromHr === 0 && fromMin === 0) ? `Send all messages from dataset? \n(Press Alt+X to stop anytime)` : `Sending messages from: ${fromHr}:${fromMin}. \nPlease confirm to continue. \n(Press Alt+X to stop anytime)`;
+    if (!confirm(confirmationMessage)) return;
 
     for (const { hh, mm, msg } of messages) {
+      if (!continueExecution) {
+        console.log('Execution stopped by user on message: ', {'hh':hh,'mm':mm,'msg':msg});
+        document.removeEventListener('keydown', haltExecution);
+        return;
+      }
       const messageHour = Number.parseInt(hh);
       const messageMinute = Number.parseInt(mm);
+      const currentTime = new Date();
+      const currentHour = currentTime.getHours();
+      const currentMinute = currentTime.getMinutes();
 
-      if (messageHour < fromHr || (messageHour === fromHr && messageMinute < fromMin)) {
-        continue;
-      }
+      if (isLateMsg(messageHour, messageMinute, fromHr, fromMin) || isLateMsg(messageHour, messageMinute, currentHour, currentMinute)) continue;
 
       typeMessage(msg);
       pressEnter();
@@ -61,7 +72,7 @@ function main() {
     }
 
     if (activeChat.getAttribute('data-type') !== 'scheduled') {
-      alert('Open the schedule messages section of chat!');
+      alert('Open the schedule messages section of chat first!');
       return;
     }
 
@@ -82,7 +93,7 @@ function main() {
 
     const [fromHr, fromMin] = minInp.split(timeSeparator).map(part => parseInt(part, 10));
 
-    if (isNaN(fromHr) || isNaN(fromMin) || fromHr < 0 || fromMin < 0) {
+    if (isNaN(fromHr) || isNaN(fromMin) || fromHr < 0 || fromHr > 23 || fromMin < 0 || fromMin > 59) {
       alert('Please enter the time in the correct format!');
       return lessgo();
     }
@@ -92,18 +103,22 @@ function main() {
 
   // Helper function to check if the given string only contains digits (0-9)
   function isNum(str) { return /^\d+$/.test(`${str}`) }
-
+  // Function to check correctness of input data
   function isDataCorrect(jsonData) {
     const errors = [];
     let rowNumber = -1;
     jsonData.forEach(data => {
       ++rowNumber;
-      if ((!isNum(`${data.hh}`) || parseInt(data.hh) < 0 || !isNum(`${data.mm}`) || parseInt(data.mm) < 0 || typeof data.msg !== 'string')) {
+      if ((
+        !isNum(`${data.hh}`) || parseInt(data.hh) < 0 || parseInt(data.hh) > 23 ||
+        !isNum(`${data.mm}`) || parseInt(data.mm) < 0 || parseInt(data.mm) > 59 ||
+        typeof data.msg !== 'string'
+      )) {
         let errorMessages = [];
-        if (!isNum(`${data.hh}`) || parseInt(data.hh) < 0) errorMessages.push('Invalid hour');
-        if (!isNum(`${data.mm}`) || parseInt(data.mm) < 0) errorMessages.push('Invalid minute');
+        if (!isNum(`${data.hh}`) || parseInt(data.hh) < 0 || parseInt(data.hh) > 23) errorMessages.push('Invalid hour');
+        if (!isNum(`${data.mm}`) || parseInt(data.mm) < 0 || parseInt(data.mm) > 59) errorMessages.push('Invalid minute');
         if (typeof data.msg !== 'string') errorMessages.push('Invalid message');
-        errors.push({ 'rowNo': rowNumber, 'errors': errorMessages,'data': data});
+        errors.push({ 'rowNo': rowNumber, 'errors': errorMessages, 'data': data });
       }
     });
     if (errors.length > 0) {
@@ -115,14 +130,36 @@ function main() {
   }
 
   const testInputs = [
-    { hh: "20", mm: "5", msg: "Heyy Jon, welcome back" },
-    { hh: "20", mm: "1", msg: "Hey Jon! It’s pretty sunny here" },
+    { hh: "22", mm: "5", msg: "Heyy Jon, welcome back" },
+    { hh: "22", mm: "1", msg: "Hey Jon! It’s pretty sunny here" },
+    { hh: "22", mm: "1", msg: "H124ey Jon! It’s pretty sunny here" },
+    { hh: "22", mm: "01", msg: "Hey1521 Jon! It’s pretty sunny here" },
+    { hh: "22", mm: "10", msg: "Hey Jo155n! It’s pretty sunnyasf here" },
+    { hh: "22", mm: "12", msg: "Hey J15115on! It’s pretty sunny heffre" },
+    { hh: "22", mm: "21", msg: "He511y J15115o1n! It’s pretty sunny here" },
+    { hh: "22", mm: "1", msg: "He1y J15115215on! It’s pretty sunny here" },
+    { hh: "22", mm: "53", msg: "He1y qwrqJ15115215on! It’s pretty sunny here" },
+    { hh: "22", mm: "6", msg: "sdgsdHe1y J15115215on! It’s pretty sunny here" },
+    { hh: "22", mm: "15", msg: "He1y J15115215on! It’s pretty sun25ny here" },
   ];
 
-  if(isDataCorrect(testInputs)) lessgo(testInputs);
+  // Flag for stopping execution
+  let continueExecution = true;
+  // Event listener to stop execution on key press (Alt + X)
+  function haltExecution(event) {
+    if (event.altKey && event.key === 'x') {
+      continueExecution = false;
+      event.stopPropagation();
+      event.preventDefault();
+    }
+  }
+  document.addEventListener('keydown', haltExecution);
+
+  if (isDataCorrect(testInputs)) lessgo(testInputs);
 }
 
 main();
 
 // For future:
+// Code should not rely on sleep function.
 // Check if the time input popup is open before entering time
